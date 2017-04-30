00e8fe643e029a696b30b1806bc534e5f28f6120
NicoAkQ <fbi007_69@hotmail.com>
2017-04-30 12:32:55 +0200

    redirection direct via adresse du site

--- monsiteinternet/monsiteinternet/urls.py
+++ monsiteinternet/monsiteinternet/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 
-
+#include('simulateurelections2017.urls')
 urlpatterns = [
 	url(r'^',include('simulateurelections2017.urls')),
     url(r'^admin/', admin.site.urls),
