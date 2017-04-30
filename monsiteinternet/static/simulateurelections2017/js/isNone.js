bc99cebbd5e703ac13fe2edf2b3026f1ee4fa799
theo vast <tvast>
2017-04-30 12:59:12 +0200

    delete ifm

--- monsiteinternet/static/simulateurelections2017/js/isNone.js
+++ monsiteinternet/static/simulateurelections2017/js/isNone.js
@@ -1,11 +1,4 @@
    function toggleDisplay() {
-        // var x = document.getElementsByClassName('isNone');
-        // console.log(x);
-        // if (x.style.display === 'none') {
-        //     x.style.display = 'block';
-        // } else {
-        //     x.style.display = 'none';
-        // }
         var x = document.getElementById("MAC"); // Using a class instead, see note below.
         x.classList.toggle('isNone');
         var x = document.getElementById("MLP"); // Using a class instead, see note below.
@@ -20,7 +13,18 @@
         x.classList.toggle('isNone');
           var x = document.getElementById("LAS"); // Using a class instead, see note below.
         x.classList.toggle('isNone');
-            var x = document.getElementById("POU"); // Using a class instead, see note below.
+        var x = document.getElementById("ABS"); // Using a class instead, see note below.
+        x.classList.toggle('isNone');
+        var x = document.getElementById("but"); // Using a class instead, see note below.
+        x.classList.toggle('isNone');
+        var x = document.getElementById("simu2"); // Using a class instead, see note below.
+        x.classList.toggle('isNone');
+        console.log('hello')
+         
+    };
+
+       function showCandidat() {
+           var x = document.getElementById("POU"); // Using a class instead, see note below.
         x.classList.toggle('isNone');
            var x = document.getElementById("ASS"); // Using a class instead, see note below.
         x.classList.toggle('isNone');
@@ -28,9 +32,6 @@
         x.classList.toggle('isNone');
           var x = document.getElementById("CHE"); // Using a class instead, see note below.
         x.classList.toggle('isNone');
-        var x = document.getElementById("ABS"); // Using a class instead, see note below.
-        x.classList.toggle('isNone');
-        var x = document.getElementById("but"); // Using a class instead, see note below.
-        x.classList.toggle('isNone');
+        
     }
 
